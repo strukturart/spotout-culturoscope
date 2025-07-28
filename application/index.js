0f4232051ccf2f7c071ab4984b22d512d3e641ca
strukturart <strukturart@gmail.com>
2025-07-28 13:35:03 +0200

    get duration and next busstation

--- application/index.js
+++ application/index.js
@@ -3,7 +3,6 @@
 import localforage from "localforage";
 
 import m from "mithril";
-import { v4 as uuidv4 } from "uuid";
 import L from "leaflet";
 import dayjs from "dayjs";
 
@@ -11,6 +10,10 @@ import "swiped-events";
 import markerIcon from "./assets/css/images/marker-icon.png";
 import markerIconRetina from "./assets/css/images/marker-icon-2x.png";
 import "font-awesome/css/font-awesome.min.css";
+
+import duration from "dayjs/plugin/duration";
+
+dayjs.extend(duration);
 
 let share_url = async (data) => {
   try {
@@ -20,14 +23,69 @@ let share_url = async (data) => {
     resultPara.textContent = `Error: ${err}`;
   }
 };
+
 const intro = document.getElementById("intro");
 
 setTimeout(() => {
   intro.style.display = "none";
-}, 8000);
+}, 5000);
 
 document.addEventListener("DOMContentLoaded", function (e) {
-  function getReferencePointFromURL() {
+  let updateTime = 3 * 60 * 60 * 1000;
+  //updateTime = 10;
+
+  let categories = ["All"];
+
+  let todosList = [];
+  let fetchError = "";
+
+  //get station around position
+  let getStation = async (lat, lng) => {
+    try {
+      const response = await fetch(
+        "https://ors.strukturart.workers.dev?start=" + lat + "&end=" + lng
+      );
+
+      if (!response.ok) throw new Error("Network response was not ok");
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Fetch-Fehler:", error);
+      return null;
+    }
+  };
+
+  let retryToload = false;
+  let getJourney = async (from, to) => {
+    try {
+      const response = await fetch(
+        `https://transport.opendata.ch/v1/connections?from=${from}&to=${to}&transportations=bus`
+      );
+
+      if (!response.ok) throw new Error("Network response was not ok");
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Fetch-Fehler:", error);
+      if (retryToload == false) {
+        let memory = updateTime;
+        updateTime = 0;
+        loadTodosAndCache();
+        retryToload = true;
+        updateTime = memory;
+      }
+      return null;
+    }
+  };
+
+  //get location from url
+  function getReferencePointFromURL(start = false) {
+    //test if location has changed
+    let lastPosition = localStorage.getItem("lat");
+
+    //default location
     let lat = 47.132344;
     let lng = 7.244604;
 
@@ -38,109 +96,141 @@ document.addEventListener("DOMContentLoaded", function (e) {
       const params = new URLSearchParams(query);
       lat = parseFloat(params.get("lat")) || lat;
       lng = parseFloat(params.get("lng")) || lng;
+
+      if (lastPosition != lat) {
+        updateTime = 0;
+        console.log("location has changed");
+      }
     }
 
-    localStorage.setItem("lat", lat);
-    localStorage.setItem("lng", lng);
+    if (start) {
+      //get closest station to current position
+      getStation(lat, lng).then((e) => {
+        let station = e.stations.find((m) => {
+          return m.id != null && m.icon == "bus";
+        });
+
+        localStorage.setItem("location_id", station?.id);
+        localStorage.setItem("location_name", station?.name);
+      });
+
+      localStorage.setItem("lat", lat);
+      localStorage.setItem("lng", lng);
+    }
 
     return L.latLng(lat, lng);
   }
 
-  const referencePoint = getReferencePointFromURL();
+  const referencePoint = getReferencePointFromURL(true);
 
-  let categories = ["All"];
+  // Hauptfunktion zum Starten
+  async function loadTodosAndCache() {
+    try {
+      const lastUpdated = await localforage.getItem("data_updated");
+      const now = Date.now();
 
-  let TWO_HOURS_MS = 2 * 60 * 60 * 1000;
-  TWO_HOURS_MS = 1000;
+      if (lastUpdated) {
+        const age = now - new Date(lastUpdated).getTime();
+        if (age < updateTime) {
+          const cachedData = await localforage.getItem("data");
+          const cachedCategories = await localforage.getItem("categories");
 
-  var Data = {
-    onremove: () => {},
-    oninit: () => {},
-    todos: {
-      list: null,
-      error: "",
-      fetch: async function () {
-        try {
-          const lastUpdated = await localforage.getItem("data_updated");
-          const now = Date.now();
+          if (cachedData && cachedCategories) {
+            todosList = cachedData;
+            categories = cachedCategories;
+            return;
+          }
+        }
+      }
 
-          if (lastUpdated) {
-            const age = now - new Date(lastUpdated).getTime();
-            if (age < TWO_HOURS_MS) {
-              const cachedData = await localforage.getItem("data");
-              if (cachedData) {
-                Data.todos.list = cachedData;
-                categories = await localforage.getItem(
-                  "categories",
-                  categories
-                );
+      // Daten von API laden
+      const response = await m.request({
+        method: "GET",
+        url: "https://yellow-base-40e2.strukturart.workers.dev/",
+      });
 
-                return;
-              }
-            }
+      todosList = response.events.filter((e) => e.address_latlng != null);
+
+      todosList.forEach((e) => {
+        const latlng = L.latLng(e.address_latlng.lat, e.address_latlng.lng);
+        const d = latlng.distanceTo(referencePoint);
+        e.distance = d;
+        e.prettyDistance =
+          d > 1000 ? (d / 1000).toFixed(1) + " km" : Math.round(d) + " m";
+        e.busStop = "";
+
+        //Categorie
+        e.event_categories_labels.forEach((label) => {
+          const trimmed = label.trim();
+          if (!categories.includes(trimmed)) {
+            categories.push(trimmed);
+          }
+        });
+
+        //get duration by event address
+        let address = e.venue_zip + " " + e.venue_city + ", " + e.venue_address;
+        getJourney(localStorage.getItem("location_id"), address).then((way) => {
+          e.duration = null;
+          if (way?.connections?.length) {
+            let d = way.connections[0].duration;
+            e.duration = d.replace(/\d{2}d/, "");
+
+            const input = e.duration;
+            const [hours, minutes, seconds] = input.split(":").map(Number);
+
+            const dur = dayjs.duration({ hours, minutes, seconds });
+            e.prettyDuration = dur.asMinutes();
+            e.transport = way;
+          }
+        });
+
+        getStation(e.address_latlng.lat, e.address_latlng.lng).then((data) => {
+          let busStop = null;
+          if (data?.stations?.length) {
+            const buses = data.stations.filter((s) => s.icon === "bus");
+            if (buses[0]?.name) busStop = buses[0].name;
           }
 
-          // Neu laden
-          const items = await m.request({
-            method: "GET",
-            url: "https://yellow-base-40e2.strukturart.workers.dev/",
-          });
+          let remove_city_string = busStop.split(",");
+          busStop = remove_city_string[1];
 
-          Data.todos.list = items.events.filter((e) => {
-            return e.address_latlng != null;
-          });
+          const original = todosList.find((a) => a.event_id === e.event_id);
+          if (original) original.busStop = busStop;
 
-          Data.todos.list.forEach((e) => {
-            const latlng = L.latLng(e.address_latlng.lat, e.address_latlng.lng);
-            const d = latlng.distanceTo(referencePoint);
-            e.distance = d;
-            e.prettyDistance =
-              d > 1000 ? (d / 1000).toFixed(1) + " km" : Math.round(d) + " m";
+          e.busStop = busStop;
+        });
+      });
 
-            //get categories
-            let allLabels = [];
+      // Nach Distanz sortieren
+      todosList.sort((a, b) => a.distance - b.distance);
 
-            Data.todos.list.forEach((e) => {
-              allLabels.push(...e.event_categories_labels); // alle Labels sammeln
-            });
+      // Cache speichern
+      await localforage.setItem("data", todosList);
+      await localforage.setItem("categories", categories);
+      await localforage.setItem("data_updated", new Date());
+    } catch (e) {
+      fetchError = e.message;
+    }
+  }
 
-            allLabels.forEach((label) => {
-              const trimmed = label.trim(); // falls nötig
-              if (!categories.includes(trimmed)) {
-                categories.push(trimmed);
-              }
-            });
-          });
+  loadTodosAndCache().then(() => {
+    m.redraw();
+  });
 
-          Data.todos.list.sort((a, b) => a.distance - b.distance);
-
-          await localforage.setItem("data", Data.todos.list);
-          await localforage.setItem("categories", categories);
-
-          await localforage.setItem("data_updated", new Date());
-        } catch (e) {
-          Data.todos.error = e.message;
-        }
-      },
-    },
-  };
+  //ugly hack
+  setTimeout(() => {
+    localforage.setItem("data", todosList);
+  }, 5000);
 
   let filter = "";
 
   var events = {
-    oninit: () => {
-      Data.todos.fetch();
-    },
+    oninit: () => {},
 
-    view: function (vnode) {
-      if (Data.todos.error) {
-        return m(".error", Data.todos.error);
-      }
-
-      if (!Data.todos.list) {
-        return m(".loading-icon");
-      }
+    view: function () {
       return m("div", [
+        m(Modal),
+
         m(
           "button",
           {
@@ -154,7 +244,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
             },
 
             oncreate: () => {
-              // Wenn schon besucht, sofort verstecken
+              //intro animation
               if (sessionStorage.getItem("visited") == "1") {
                 intro.style.display = "none";
               } else {
@@ -165,6 +255,16 @@ document.addEventListener("DOMContentLoaded", function (e) {
             },
           },
           "Filter"
+        ),
+
+        m(
+          "div",
+          {
+            oncreate: (vnode) => {
+              vnode.dom.style.display = "none";
+            },
+          },
+          localStorage.getItem("location_name")
         ),
 
         m(
@@ -195,7 +295,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
         m(
           "div",
           { class: "row between-md" },
-          Data.todos.list.map(function (item) {
+          todosList.map(function (item) {
             if (
               filter != "All" &&
               filter != "" &&
@@ -211,8 +311,19 @@ document.addEventListener("DOMContentLoaded", function (e) {
                 dayjs(item.event_dates[0].start_date).format("DD.MM.YY HH:mm")
               ),
               m("div", item.prettyDistance),
-              m("img", { src: item.image_url, loading: "lazy" }),
+              item.duration
+                ? m(
+                    "div",
+                    {
+                      oncreate: (vnode) => {
+                        vnode.dom.innerText = item.prettyDuration + "min";
+                      },
+                    },
+                    item.duration
+                  )
+                : null,
 
+              m("img", { src: item.image_url, loading: "lazy" }),
               m("div", { class: "action-buttons row" }, [
                 m(
                   "a",
@@ -227,6 +338,27 @@ document.addEventListener("DOMContentLoaded", function (e) {
                     m("i", {
                       class: "col-xs-2 fa fa-map fa-2x",
                       "aria-hidden": "true",
+                    }),
+                  ]
+                ),
+                m(
+                  "a",
+                  {
+                    href: "#modal",
+                    onclick: (e) => {
+                      e.preventDefault();
+                      Modal.toggle(true, {
+                        title: "🚌 Bus",
+                        message:
+                          "you will find a bus stop near you that will take you to the event in <strong>" +
+                          item.prettyDuration +
+                          " minutes</strong>.",
+                      });
+                    },
+                  },
+                  [
+                    m("i", {
+                      class: "col-xs-2 fa fa-bus fa-2x",
                     }),
                   ]
                 ),
@@ -267,11 +399,46 @@ document.addEventListener("DOMContentLoaded", function (e) {
     },
   };
 
+  const Modal = {
+    visible: false,
+    data: {},
+
+    toggle(state, data = {}) {
+      Modal.visible = state;
+      Modal.data = data;
+    },
+
+    // Modal.js
+    view(vnode) {
+      if (!Modal.visible) return null;
+      const { title, message } = Modal.data;
+
+      return m(
+        "dialog[open]",
+        {
+          onclick: (e) => {
+            if (e.target.tagName === "DIALOG") Modal.toggle(false);
+          },
+        },
+        m("article", [
+          m("header", [
+            m("button", {
+              onclick: () => {
+                Modal.toggle(false);
+              },
+              "aria-label": "Close",
+              "rel": "prev",
+            }),
+            m("p", m("strong", title)),
+          ]),
+          m("p", [m.trust(message)]),
+        ])
+      );
+    },
+  };
+
   let map_container;
   var map = {
-    oninit: () => {
-      console.log(getReferencePointFromURL());
-    },
     onremove: () => {
       map_container = null;
     },
@@ -280,12 +447,13 @@ document.addEventListener("DOMContentLoaded", function (e) {
         id: "map-container",
         class: "",
         oncreate: () => {
-          let a = getReferencePointFromURL();
-          let latlng_url = [a.lat, a.lng];
+          let lat = localStorage.getItem("lat");
+          let lng = localStorage.getItem("lng");
+
           map_container = L.map("map-container", {
             keyboard: true,
             zoomControl: false,
-          }).setView(latlng_url, 16);
+          }).setView([lat, lng], 16);
 
           L.Icon.Default.prototype.options.shadowUrl = "";
           L.Icon.Default.prototype.options.iconUrl = markerIcon;
@@ -296,7 +464,15 @@ document.addEventListener("DOMContentLoaded", function (e) {
               '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
           }).addTo(map_container);
 
-          Data.todos.list.map((e) => {
+          //users location
+          //without geolocatioon api
+          //qr-scan->url
+          let myMarker = L.marker([lat, lng]);
+          myMarker.addTo(map_container).bindTooltip("You");
+          myMarker.openTooltip();
+
+          //get events location and create markers
+          todosList.map((e) => {
             let marker = L.marker([
               e.address_latlng.lat,
               e.address_latlng.lng,
@@ -322,11 +498,11 @@ document.addEventListener("DOMContentLoaded", function (e) {
       return m("div", { class: "row center-xs", id: "about" }, [
         m("div", { class: "col-xs-12 col-md-7 text" }, [
           m.trust(
-            "Diese Website zeigt dir, welche Veranstaltungen in der Nähe eines der 'Kultursäulen' der Stadt Biel/Bienne stattfinden.Sie wurde im Rahmen des Projekts <a href='https://spotin.ch' target:'_blank'>Spotin</a> erstellt, das von der Stadt Biel/Bienne initiiert wurde.Die Veranstaltungsdaten werden freundlicherweise von CultuScope zur Verfügung gestellt."
+            "Diese Website zeigt dir, welche Veranstaltungen in der Nähe eines der 'Kultursäulen' der Stadt Biel/Bienne stattfinden.Sie wurde im Rahmen des Projekts <a href='https://spotin.ch' target:'_blank'>Spotin</a> erstellt, das von der Stadt Biel/Bienne initiiert wurde.Die Veranstaltungsdaten werden von <a target:'_blank' href='https://www.culturoscope.ch/agenda'>Culturocope</a> zur Verfügung gestellt."
           ),
 
           m.trust(
-            "<br><br>Ce site vous permet de découvrir les événements qui ont lieu à proximité de l’un des piliers culturels de la ville de Bienne. Il a été réalisé dans le cadre du projet Spotin, une initiative de la Ville de Bienne.Les données des événements sont gracieusement fournies par CultuScope"
+            "<br><br>Ce site vous permet de découvrir les événements qui ont lieu à proximité de l’un des piliers culturels de la ville de Bienne. Il a été réalisé dans le cadre du projet <a href='https://spotin.ch' target:'_blank'>Spotin</a>, une initiative de la Ville de Bienne.Les données des événements sont fournies par <a target:'_blank' href='https://www.culturoscope.ch/agenda'>Culturocope</a>"
           ),
           m.trust("<br><br><strong>Links</strong><br>"),
           m("ul", [
